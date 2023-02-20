8675518eb4107bd4c548f6678fd09d01940cc01e
Valentina <vh010200@gmail.com>
2023-02-20 19:09:33 +0100

    global users endpoint URL added

--- Habit_Tracker/Habit_Tracker/urls.py
+++ Habit_Tracker/Habit_Tracker/urls.py
@@ -1,19 +1,19 @@
 from django.urls import path, include
+from rest_framework import routers
+from People import view
 from Rewards import views
-from Users.models import Person
 from Users.views import get_user_habits
-from Users.serializers import PersonSerializer
-from rest_framework import routers, viewsets
+
 
 
 
 router = routers.DefaultRouter()
+router.register(r'people', view.PersonViewSet)
 router.register(r'rewards', views.RewardsViewset)
 
 urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    # path('users/', get_person_data, name='get_person_data'),
     path('users/<int:id>/habits', get_user_habits)
 
 ]
