1adc31c9ae4c0477d1cf485b3edb05bc41c2899f
Valentina <vh010200@gmail.com>
2023-02-21 14:49:21 +0100

    users folder deleted

--- Habit_Tracker/Users/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from Users.models import Person
-
-class PersonSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Person
-        fields = ('name', 'username', 'habit')
