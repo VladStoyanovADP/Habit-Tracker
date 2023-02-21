6f59e12aa4169b9a635a1c6edffd3b8658376b51
Vlad <vladstoyanoff@gmail.com>
2023-02-21 13:26:46 +0000

    restore RewardsViewSet

--- Habit_Tracker/Habit_Tracker/urls.py
+++ Habit_Tracker/Habit_Tracker/urls.py
@@ -2,13 +2,13 @@ from django.urls import path, include
 from rest_framework import routers
 from Users.views import get_user_habits
 from People import views as PersonViewSet
+from People import views as RewardsViewSet
 from Achievements import views as AchievementsViewSet
-
 
 router = routers.DefaultRouter()
 router.register(r'people', PersonViewSet)
+router.register(r'rewards', RewardsViewSet)
 router.register(r'achievements', AchievementsViewSet)
-
 
 urlpatterns = [
     path('', include(router.urls)),
