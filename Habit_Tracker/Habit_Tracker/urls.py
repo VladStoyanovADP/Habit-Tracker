66099193f3e1a9a307dd74aa49832608644464ad
Valentina <vh010200@gmail.com>
2023-02-21 14:50:48 +0100

    urls edit

--- Habit_Tracker/Habit_Tracker/urls.py
+++ Habit_Tracker/Habit_Tracker/urls.py
@@ -2,7 +2,6 @@ from django.urls import path, include
 from rest_framework import routers
 from Achievements import views as AchievementsViewSet
 from People import views
-from Users.views import get_user_habits
 
 
 
@@ -13,7 +12,5 @@ router.register(r'achievements', AchievementsViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('users/<int:id>/habits', get_user_habits)
-
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
