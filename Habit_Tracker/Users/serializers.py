8d992f43e8c6723ef8a7150a369b2812f8264f1a
Valentina <vh010200@gmail.com>
2023-02-22 16:47:49 +0100

    refactored to display user_id instead of url

--- Habit_Tracker/Users/serializers.py
+++ Habit_Tracker/Users/serializers.py
@@ -7,11 +7,19 @@ class PersonSerializer(serializers.HyperlinkedModelSerializer):
         fields = ('name', 'username', 'password', 'email', 'avatar_url', 'description', 'created_at', 'currency', 'phone', 'achievements')
 
 class RewardsSerializer(serializers.HyperlinkedModelSerializer):
+    user_id = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field="id"
+    )
     class Meta:
         model = Rewards
         fields = ['rewards_name', 'rewards_description', 'rewards_cost', 'user_id']
         
 class HabitsSerializer(serializers.HyperlinkedModelSerializer):
+    user_id = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field="id"
+    )
     class Meta:
         model = Habits
         fields = ['habit_name', 'habit_category', 'habit_type', 'habit_streak', 'user_id']
