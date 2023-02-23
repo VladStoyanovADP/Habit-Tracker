e5e8ccdbc7e8e25b31a0047f468edd2d968433de
Valentina <vh010200@gmail.com>
2023-02-23 17:07:27 +0100

    serializers restored

--- Habit_Tracker/Users/serializers.py
+++ Habit_Tracker/Users/serializers.py
@@ -21,7 +21,6 @@ class TokenViewSerializer(TokenObtainPairSerializer):
     @classmethod
     def get_token(cls, user):
         token = super().get_token(user)
-
         token['username'] = user.username
         token['email'] = user.email
         return token
