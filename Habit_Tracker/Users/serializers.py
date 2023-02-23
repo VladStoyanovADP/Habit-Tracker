0f8208e4b65e7473f0061fef878e12591e1b5022
Vlad <vladstoyanoff@gmail.com>
2023-02-23 15:16:36 +0000

    tests for /users and /users/:user_id

--- Habit_Tracker/Users/serializers.py
+++ Habit_Tracker/Users/serializers.py
@@ -4,7 +4,7 @@ from .models import Person, Rewards, Habits
 class PersonSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Person
-        fields = ('name', 'username', 'password', 'email', 'avatar_url', 'description', 'created_at', 'currency', 'phone', 'achievements')
+        fields = ('name', 'username', 'password', 'email', 'avatar_url', 'description', 'created_at', 'currency', 'phone')
 
 class RewardsSerializer(serializers.HyperlinkedModelSerializer):
     user_id = serializers.SlugRelatedField(
