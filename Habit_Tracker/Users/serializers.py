c810daff820e343a1d342b1b007c2bb8b10d1895
Vlad <vladstoyanoff@gmail.com>
2023-02-23 15:59:36 +0000

    achievements

--- Habit_Tracker/Users/serializers.py
+++ Habit_Tracker/Users/serializers.py
@@ -1,17 +1,26 @@
 from rest_framework import serializers
 from .models import Person, Rewards, Habits
 
-class PersonSerializer(serializers.ModelSerializer):
+class PersonSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Person
-        fields = ('name', 'username', 'password', 'email', 'avatar_url', 'description', 'created_at', 'currency', 'phone')
+        fields = ('name', 'username', 'password', 'email', 'avatar_url',
+                  'description', 'created_at', 'currency', 'phone', 'achievements')
 
-class RewardsSerializer(serializers.ModelSerializer):
+class RewardsSerializer(serializers.HyperlinkedModelSerializer):
+    user_id = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field="id"
+    )
     class Meta:
         model = Rewards
         fields = ['rewards_name', 'rewards_description', 'rewards_cost', 'user_id']
         
-class HabitsSerializer(serializers.ModelSerializer):
+class HabitsSerializer(serializers.HyperlinkedModelSerializer):
+    user_id = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field="id"
+    )
     class Meta:
         model = Habits
         fields = ['habit_name', 'habit_category', 'habit_type', 'habit_streak', 'user_id']
