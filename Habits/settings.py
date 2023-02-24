563656607e66e565c340f536974f8c05a1da8c9c
Valentina <vh010200@gmail.com>
2023-02-24 12:46:34 +0100

    staticroot

--- Habits/settings.py
+++ Habits/settings.py
@@ -125,3 +125,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
