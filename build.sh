5559f4e8ed1b995e168d01b00d66cfc4000c802e
Valentina <vh010200@gmail.com>
2023-02-27 12:19:24 +0100

    #12

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 
-PGPASSWORD=oPXoTh4xYnYjO5Kr2uTcNtUw70HufxrO psql  -U host_test_user -h dpg-cfs9odhmbjshr9nevltg-a -c "DROP DATABASE IF EXISTS host_test"
-PGPASSWORD=oPXoTh4xYnYjO5Kr2uTcNtUw70HufxrO psql  -U host_test_user -h dpg-cfs9odhmbjshr9nevltg-a -c "CREATE DATABASE host_test"
+PGPASSWORD=oPXoTh4xYnYjO5Kr2uTcNtUw70HufxrO psql -U host_test_user -h dpg-cfs9odhmbjshr9nevltg-a host_test -c "DROP DATABASE IF EXISTS host_test"
+PGPASSWORD=oPXoTh4xYnYjO5Kr2uTcNtUw70HufxrO psql -U host_test_user -h dpg-cfs9odhmbjshr9nevltg-a host_test -c "CREATE DATABASE host_test"
 
 python manage.py makemigrations Achievements
 python manage.py makemigrations Users
