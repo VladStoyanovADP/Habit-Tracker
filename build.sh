544833f63df5e0f915ecfa54032ae47b635cf7db
Valentina <vh010200@gmail.com>
2023-02-27 11:03:02 +0100

    attempt n2

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 
+export PGPASSWORD = oPXoTh4xYnYjO5Kr2uTcNtUw70HufxrO
 psql -U host_test_user -h dpg-cfs9odhmbjshr9nevltg-a -c "DROP DATABASE IF EXISTS host_test"
 psql -U host_test_user -h dpg-cfs9odhmbjshr9nevltg-a -c "CREATE DATABASE host_test"
 
