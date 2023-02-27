c4844564c131361cd3e6c1b63e1ae8fcde20d1f5
Valentina <vh010200@gmail.com>
2023-02-27 12:40:01 +0100

    #19?

--- build.sh
+++ build.sh
@@ -4,12 +4,9 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 
-PGPASSWORD=oPXoTh4xYnYjO5Kr2uTcNtUw70HufxrO psql -U host_test_user -h dpg-cfs9odhmbjshr9nevltg-a host_test -c 'DROP TABLE IF EXISTS django_migrations'
-PGPASSWORD=oPXoTh4xYnYjO5Kr2uTcNtUw70HufxrO psql -U host_test_user -h dpg-cfs9odhmbjshr9nevltg-a host_test -c 'DROP TABLE IF EXISTS "Users_habits"'
-PGPASSWORD=oPXoTh4xYnYjO5Kr2uTcNtUw70HufxrO psql -U host_test_user -h dpg-cfs9odhmbjshr9nevltg-a host_test -c 'DROP TABLE IF EXISTS "Users_rewards"'
-PGPASSWORD=oPXoTh4xYnYjO5Kr2uTcNtUw70HufxrO psql -U host_test_user -h dpg-cfs9odhmbjshr9nevltg-a host_test -c 'DROP TABLE IF EXISTS "Users_person_achievements"'
-PGPASSWORD=oPXoTh4xYnYjO5Kr2uTcNtUw70HufxrO psql -U host_test_user -h dpg-cfs9odhmbjshr9nevltg-a host_test -c 'DROP TABLE IF EXISTS "Users_person"'
-PGPASSWORD=oPXoTh4xYnYjO5Kr2uTcNtUw70HufxrO psql -U host_test_user -h dpg-cfs9odhmbjshr9nevltg-a host_test -c 'DROP TABLE IF EXISTS "Achievements_achievements"'
+PGPASSWORD=oPXoTh4xYnYjO5Kr2uTcNtUw70HufxrO psql -U host_test_user -h dpg-cfs9odhmbjshr9nevltg-a host_test -c 'DROP SCHEMA public CASCADE;'
+
+
 
 python manage.py makemigrations Achievements
 python manage.py makemigrations Users
