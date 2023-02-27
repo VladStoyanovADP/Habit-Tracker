20a14dc898a5fc18542b29e90cff617a2b2d17f0
Valentina <vh010200@gmail.com>
2023-02-27 10:58:22 +0100

    another attempt at build.sh

--- build.sh
+++ build.sh
@@ -4,30 +4,8 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 
-python << EOF
-import psycopg2
-import os
-
-dbname = os.environ.get('host_test')
-dbuser = os.environ.get('host_test_user')
-dbpassword = os.environ.get('oPXoTh4xYnYjO5Kr2uTcNtUw70HufxrO')
-dbhost = os.environ.get('dpg-cfs9odhmbjshr9nevltg-a')
-
-conn = psycopg2.connect(
-    dbname=dbname, user=dbuser, password=dbpassword, host=dbhost, port='5432'
-)
-conn.autocommit = True
-
-cursor = conn.cursor()
-cursor.execute("DROP DATABASE IF EXISTS habit_tracker_db")
-
-sql = '''CREATE DATABASE habit_tracker_db''';
-
-cursor.execute(sql)
-print("Database created successfully......")
-
-conn.close()
-EOF
+psql -U host_test_user -h dpg-cfs9odhmbjshr9nevltg-a -c "DROP DATABASE IF EXISTS host_test"
+psql -U host_test_user -h dpg-cfs9odhmbjshr9nevltg-a -c "CREATE DATABASE host_test"
 
 python manage.py makemigrations Achievements
 python manage.py makemigrations Users
