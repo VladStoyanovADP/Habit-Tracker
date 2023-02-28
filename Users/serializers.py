6440fdb6306c2ce59d259d4f537bc2ed736115f1
Valentina <vh010200@gmail.com>
2023-02-28 11:47:57 +0100

    ids

--- Users/serializers.py
+++ Users/serializers.py
@@ -5,17 +5,17 @@ from .models import Person, Rewards, Habits
 class PersonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Person
-        fields = ('name', 'username', 'password', 'email', 'avatar_url', 'description', 'created_at', 'currency', 'phone', 'achievements')
+        fields = ('id', 'name', 'username', 'password', 'email', 'avatar_url', 'description', 'created_at', 'currency', 'phone', 'achievements')
 
 class RewardsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Rewards
-        fields = ['rewards_name', 'rewards_description', 'rewards_cost', 'user_id']
+        fields = ['id', 'rewards_name', 'rewards_description', 'rewards_cost', 'user_id']
         
 class HabitsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Habits
-        fields = ['habit_name', 'habit_category', 'habit_type', 'habit_streak', 'user_id']
+        fields = ['id', 'habit_name', 'habit_category', 'habit_type', 'habit_streak', 'user_id']
 
 class TokenViewSerializer(TokenObtainPairSerializer):
     @classmethod
