f56502c18756439f9dbe61dbb4baa32938af5cd6
Valentina <vh010200@gmail.com>
2023-02-28 11:32:01 +0100

    all fields

--- Users/serializers.py
+++ Users/serializers.py
@@ -15,7 +15,8 @@ class RewardsSerializer(serializers.ModelSerializer):
 class HabitsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Habits
-        fields = ['habit_id_id', 'habit_name', 'habit_category', 'habit_type', 'habit_streak', 'user_id']
+#        fields = ['habit_id_id', 'habit_name', 'habit_category', 'habit_type', 'habit_streak', 'user_id']
+        fields = '__all__'
 
 class TokenViewSerializer(TokenObtainPairSerializer):
     @classmethod
