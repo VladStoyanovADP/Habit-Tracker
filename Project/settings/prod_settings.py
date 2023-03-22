3125d247b4c665213a6439efa449f489d6cfb8f0
dvbenson <112098121+dvbenson@users.noreply.github.com>
2023-03-22 13:42:14 +0000

    host update

--- Project/settings/prod_settings.py
+++ Project/settings/prod_settings.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 SECRET_KEY = str(os.getenv("SECRET_KEY"))
 DEBUG = False
-ALLOWED_HOSTS = ['host-test-yju8.onrender.com']
+ALLOWED_HOSTS = ['host-test-yju8.onrender.com', 'final-api.onrender.com', 'authentication-test-7108.onrender.com' ]
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600)
 }
