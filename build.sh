290ce966824279d3ffbc91bb7a40c1144b15687d
dvbenson <112098121+dvbenson@users.noreply.github.com>
2023-03-23 15:33:31 +0000

    slight updates for hosting

--- build.sh
+++ build.sh
@@ -4,6 +4,22 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 
+PGPASSWORD=oPXoTh4xYnYjO5Kr2uTcNtUw70HufxrO psql -U host_test_user -h dpg-cfs9odhmbjshr9nevltg-a host_test -c 'DROP TABLE IF EXISTS django_migrations'
+PGPASSWORD=oPXoTh4xYnYjO5Kr2uTcNtUw70HufxrO psql -U host_test_user -h dpg-cfs9odhmbjshr9nevltg-a host_test -c "DROP TABLE IF EXISTS django_admin_log"
+PGPASSWORD=oPXoTh4xYnYjO5Kr2uTcNtUw70HufxrO psql -U host_test_user -h dpg-cfs9odhmbjshr9nevltg-a host_test -c "DROP TABLE IF EXISTS django_content_type CASCADE"
+PGPASSWORD=oPXoTh4xYnYjO5Kr2uTcNtUw70HufxrO psql -U host_test_user -h dpg-cfs9odhmbjshr9nevltg-a host_test -c "DROP TABLE IF EXISTS django_session"
+PGPASSWORD=oPXoTh4xYnYjO5Kr2uTcNtUw70HufxrO psql -U host_test_user -h dpg-cfs9odhmbjshr9nevltg-a host_test -c "DROP TABLE IF EXISTS auth_permission CASCADE"
+PGPASSWORD=oPXoTh4xYnYjO5Kr2uTcNtUw70HufxrO psql -U host_test_user -h dpg-cfs9odhmbjshr9nevltg-a host_test -c "DROP TABLE IF EXISTS auth_group CASCADE"
+PGPASSWORD=oPXoTh4xYnYjO5Kr2uTcNtUw70HufxrO psql -U host_test_user -h dpg-cfs9odhmbjshr9nevltg-a host_test -c "DROP TABLE IF EXISTS auth_group_permissions CASCADE"
+PGPASSWORD=oPXoTh4xYnYjO5Kr2uTcNtUw70HufxrO psql -U host_test_user -h dpg-cfs9odhmbjshr9nevltg-a host_test -c "DROP TABLE IF EXISTS auth_user CASCADE"
+PGPASSWORD=oPXoTh4xYnYjO5Kr2uTcNtUw70HufxrO psql -U host_test_user -h dpg-cfs9odhmbjshr9nevltg-a host_test -c "DROP TABLE IF EXISTS auth_user_groups CASCADE"
+PGPASSWORD=oPXoTh4xYnYjO5Kr2uTcNtUw70HufxrO psql -U host_test_user -h dpg-cfs9odhmbjshr9nevltg-a host_test -c "DROP TABLE IF EXISTS auth_user_user_permissions CASCADE"
+PGPASSWORD=oPXoTh4xYnYjO5Kr2uTcNtUw70HufxrO psql -U host_test_user -h dpg-cfs9odhmbjshr9nevltg-a host_test -c 'DROP TABLE IF EXISTS "Users_habits"'
+PGPASSWORD=oPXoTh4xYnYjO5Kr2uTcNtUw70HufxrO psql -U host_test_user -h dpg-cfs9odhmbjshr9nevltg-a host_test -c 'DROP TABLE IF EXISTS "Users_rewards"'
+PGPASSWORD=oPXoTh4xYnYjO5Kr2uTcNtUw70HufxrO psql -U host_test_user -h dpg-cfs9odhmbjshr9nevltg-a host_test -c 'DROP TABLE IF EXISTS "Users_person_achievements"'
+PGPASSWORD=oPXoTh4xYnYjO5Kr2uTcNtUw70HufxrO psql -U host_test_user -h dpg-cfs9odhmbjshr9nevltg-a host_test -c 'DROP TABLE IF EXISTS "Users_person"'
+PGPASSWORD=oPXoTh4xYnYjO5Kr2uTcNtUw70HufxrO psql -U host_test_user -h dpg-cfs9odhmbjshr9nevltg-a host_test -c 'DROP TABLE IF EXISTS "Achievements_achievements"'
+
 python manage.py makemigrations Achievements
 python manage.py makemigrations Users
 python manage.py migrate
